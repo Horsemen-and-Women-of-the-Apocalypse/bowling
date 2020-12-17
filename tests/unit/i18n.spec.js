d503631fe67b06944da3680824c5411e3a596085
Remy.c <remychagnas@hotmail.fr>
2020-12-17 17:27:39 +0100

    add lint from develop

--- tests/unit/i18n.spec.js
+++ tests/unit/i18n.spec.js
@@ -1,4 +1,4 @@
-import originalTranslations from "../../src/assets/lang"
+import originalTranslations from '../../src/assets/lang'
 
 /**
  * Deeply remove values contained by an object
@@ -6,40 +6,38 @@ import originalTranslations from "../../src/assets/lang"
  * @param obj Target
  */
 const removeValues = (obj) => {
-    if (!(obj instanceof Object)) {
-        return;
-    }
+  if (!(obj instanceof Object)) {
+    return
+  }
 
-    for (const key in obj) {
+  for (const key in obj) {
+    // @ts-ignore
+    if (obj[key] instanceof Object) {
+      // @ts-ignore
+      removeValues(obj[key])
+    } else {
+      // @ts-ignore
+      obj[key] = undefined
+    }
+  }
+}
+
+describe('i18n', () => {
+  test('Assert contents', () => {
+    // Remove values to test only keys
+    const translations = JSON.parse(JSON.stringify(originalTranslations))
+    removeValues(translations)
+
+    for (const lang in translations) {
+      for (const otherLang in translations) {
+        // Skip comparison between the same language
+        if (lang === otherLang) {
+          continue
+        }
+
         // @ts-ignore
-        if (obj[key] instanceof Object) {
-            // @ts-ignore
-            removeValues(obj[key]);
-        } else {
-            // @ts-ignore
-            obj[key] = undefined;
-        }
+        expect(translations[lang]).toStrictEqual(translations[otherLang])
+      }
     }
-};
-
-
-
-describe("i18n", () => {
-    test("Assert contents", () => {
-        // Remove values to test only keys
-        const translations = JSON.parse(JSON.stringify(originalTranslations));
-        removeValues(translations);
-
-        for (const lang in translations) {
-            for (const otherLang in translations) {
-                // Skip comparison between the same language
-                if (lang === otherLang) {
-                    continue;
-                }
-
-                // @ts-ignore
-                expect(translations[lang]).toStrictEqual(translations[otherLang]);
-            }
-        }
-    });
-});
+  })
+})
