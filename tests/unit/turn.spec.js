2197999bcf193e78e72af31cbda0499142e2e35a
Remy.c <remychagnas@hotmail.fr>
2020-12-24 17:24:55 +0100

    fix lint

--- tests/unit/turn.spec.js
+++ tests/unit/turn.spec.js
@@ -1,45 +1,41 @@
 import { mount } from '@vue/test-utils'
 import Turn from '../../src/views/Turn.vue'
 
-
 describe('turn', () => {
-    test('add', () => {
-  
-        const $t = () => {}
-        
-        const wrapper = mount(Turn, {
-            mocks:{ $t }
-        })
-        
-        wrapper.find('md-button#add').trigger('click')
-        
-        expect(wrapper.vm.turnCount).toBe(2)
+  test('add', () => {
+    const $t = () => {}
+
+    const wrapper = mount(Turn, {
+      mocks: { $t }
     })
-      
-    test('minus', () => {
-        
-        const $t = () => {}
-      
-        const wrapper = mount(Turn, {
-          mocks:{ $t }
-        })
-      
-        wrapper.find('md-button#add').trigger('click')
-        wrapper.find('md-button#minus').trigger('click')
-      
-        expect(wrapper.vm.turnCount).toBe(1)
-      })
-      
-    test('minusTo0', () => {
-        
-        const $t = () => {}
-      
-        const wrapper = mount(Turn, {
-          mocks:{ $t }
-        })
-      
-        wrapper.find('md-button#minus').trigger('click')
-      
-        expect(wrapper.vm.turnCount).toBe(1)
+
+    wrapper.find('md-button#add').trigger('click')
+
+    expect(wrapper.vm.turnCount).toBe(2)
+  })
+
+  test('minus', () => {
+    const $t = () => {}
+
+    const wrapper = mount(Turn, {
+      mocks: { $t }
     })
+
+    wrapper.find('md-button#add').trigger('click')
+    wrapper.find('md-button#minus').trigger('click')
+
+    expect(wrapper.vm.turnCount).toBe(1)
+  })
+
+  test('minusTo0', () => {
+    const $t = () => {}
+
+    const wrapper = mount(Turn, {
+      mocks: { $t }
+    })
+
+    wrapper.find('md-button#minus').trigger('click')
+
+    expect(wrapper.vm.turnCount).toBe(1)
+  })
 })
