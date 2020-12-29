f289397ed47cf95837d664fc17badf9aa4bffef8
tom.mansion <tomansion@yahoo.fr>
2020-12-29 13:46:39 +0100

    component pass tests ok

--- src/assets/lang/index.js
+++ src/assets/lang/index.js
@@ -1,4 +1,5 @@
 import home from './home'
+import PlayerListCreator from './PlayerListCreator'
 import merge from 'deepmerge'
 
-export default merge.all([home])
+export default merge.all([home, PlayerListCreator])
---
+++ src/views/PlayerListCreator.vue
@@ -0,0 +1,163 @@
+<template>
+  <div id="PlayerListCreator">
+    <div id="title">
+      <h1>
+        {{ $t("PlayerListCreator.title1_1") }}
+        <span>{{ $t("PlayerListCreator.title1_2") }}</span> ?
+      </h1>
+      <h2>
+        {{ $t("PlayerListCreator.title2") }}
+      </h2>
+    </div>
+    <div id="playerList">
+      <!-- Created player list -->
+      <div
+        class="player"
+        v-for="(player, i) in players"
+        :key="i"
+        :id="'player_' + (i + 1)"
+      >
+        <div class="name">
+          {{ player.getName() }}
+        </div>
+        <div class="removeBtn">
+          <md-button
+            v-on:click="removePlayer(i)"
+            class="removePlayerBtn md-icon-button"
+          >
+            <md-icon>remove</md-icon>
+          </md-button>
+        </div>
+      </div>
+
+      <!-- Add player line -->
+      <form class="player add" @submit.prevent="">
+        <div class="name">
+          <input type="text" name="newPlayerName" v-model="newPlayerName" />
+        </div>
+        <div class="addBtn">
+          <md-button
+            type="submit"
+            id="addPlayerBtn"
+            @click="addPlayer"
+            class="md-icon-button"
+          >
+            <md-icon>add</md-icon>
+          </md-button>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+import Player from '../objets/player'
+
+export default {
+  name: 'Turn',
+  data: () => ({
+    newPlayerName: '',
+    players: []
+  }),
+  methods: {
+    addPlayer (e) {
+      // Add a new player object to the player list
+      if (this.newPlayerName.length > 0) {
+        // check for duplicates
+        if (
+          this.players.find((p) => p.getName() === this.newPlayerName) ===
+          undefined
+        ) {
+          this.players.push(new Player(this.newPlayerName))
+          this.newPlayerName = ''
+        }
+      }
+      e.preventDefault()
+    },
+    removePlayer (index) {
+      // Remove a player from his index pos
+      this.players.splice(index, 1)
+    },
+    getPlayers () {
+      // Method to get the player list
+      return this.players
+    }
+  }
+}
+</script>
+
+<style scoped>
+#PlayerListCreator {
+  background-color: #bfb35a;
+  height: 100%;
+}
+
+/* Title */
+#title {
+  background-color: #bdbf5a;
+  padding: 10px;
+  text-align: left;
+}
+#title h1 {
+  color: #8c2318;
+  margin-bottom: 0px;
+}
+#title h1 span {
+  color: #88a65e;
+}
+#title h2 {
+  color: #8c2318;
+  font-size: 1em;
+  margin-top: 0px;
+  font-weight: 100;
+}
+
+/* Players */
+.player {
+  padding: 2%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.player:nth-child(2n) {
+  background-color: #bdbf5a;
+}
+.player .name {
+  border-bottom: solid 3px #88a65e;
+  margin-left: 4%;
+  font-size: 1.5em;
+}
+
+.player.add {
+  background-color: #f8d562;
+}
+.player.add input {
+  border: 0;
+  outline: 0;
+  padding: 7px 0;
+  background: transparent;
+}
+
+/* Buttons */
+.md-button {
+  background-color: #687555 !important;
+}
+.md-icon {
+  color: white !important;
+}
+@media only screen and (max-device-width: 480px) {
+  .turn {
+    font-size: 1em;
+  }
+}
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+  /* Chrome, Safari, Edge, Opera support */
+  -webkit-appearance: none;
+}
+
+input[type="number"] {
+  /* Firefox support */
+  -moz-appearance: textfield;
+}
+</style>
