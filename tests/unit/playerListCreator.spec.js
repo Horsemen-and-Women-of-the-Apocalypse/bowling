1ea1781b1084a6fffea12c71a1b61719c1a0d1dd
tom.mansion <tomansion@yahoo.fr>
2020-12-29 19:35:00 +0100

    tested the removePlayer index verification

--- tests/unit/playerListCreator.spec.js
+++ tests/unit/playerListCreator.spec.js
@@ -130,5 +130,43 @@ describe('PlayerListCreator component', () => {
     await wrapper.find('#player_1 .removePlayerBtn').trigger('click')
     expect(wrapper.vm.getPlayers().length).toBe(1)
     expect(wrapper.vm.getPlayers()[0].getName()).toBe(newPlayerName2)
+
+    // remove Second player
+    await wrapper.find('#player_1 .removePlayerBtn').trigger('click')
+    expect(wrapper.vm.getPlayers().length).toBe(0)
+  })
+  test('Remove a non existing player', async () => {
+    const wrapper = mountComponent()
+
+    // 0 players
+    expect(wrapper.vm.getPlayers().length).toBe(0)
+
+    // 1 player
+    const newPlayerName1 = 'tata toto'
+    await wrapper.find(newPlayerNameInputSelector).setValue(newPlayerName1)
+    await wrapper.find(addNewPlayerBtnSelector).trigger('click')
+
+    // 2nd player
+    const newPlayerName2 = 'tutu tonton'
+    await wrapper.find(newPlayerNameInputSelector).setValue(newPlayerName2)
+    await wrapper.find(addNewPlayerBtnSelector).trigger('click')
+
+    expect(wrapper.vm.getPlayers().length).toBe(2)
+    expect(wrapper.vm.getPlayers()[0].getName()).toBe(newPlayerName1)
+    expect(wrapper.vm.getPlayers()[1].getName()).toBe(newPlayerName2)
+
+    // remove player -1
+    wrapper.vm.removePlayer(-1)
+
+    expect(wrapper.vm.getPlayers().length).toBe(2) // no changes
+    expect(wrapper.vm.getPlayers()[0].getName()).toBe(newPlayerName1)
+    expect(wrapper.vm.getPlayers()[1].getName()).toBe(newPlayerName2)
+
+    // remove player len +1
+    wrapper.vm.removePlayer(3)
+
+    expect(wrapper.vm.getPlayers().length).toBe(2) // no changes
+    expect(wrapper.vm.getPlayers()[0].getName()).toBe(newPlayerName1)
+    expect(wrapper.vm.getPlayers()[1].getName()).toBe(newPlayerName2)
   })
 })
