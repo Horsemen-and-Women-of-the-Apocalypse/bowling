001f377ca3bbfc5636ff6987a41285f7012d49d9
Remy.c <remychagnas@hotmail.fr>
2020-12-30 16:03:01 +0100

    add about and lang files

--- src/assets/lang/index.js
+++ src/assets/lang/index.js
@@ -1,7 +1,8 @@
 import home from './home'
 import turn from './turn'
 import pins from './pins'
+import about from './about'
 import playerListCreator from './playerListCreator'
 import merge from 'deepmerge'
 
-export default merge.all([home, turn, pins, playerListCreator])
+export default merge.all([home, turn, pins, playerListCreator, about])
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,32 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <div id="title">{{ $t('about.title')}}</div>
+    <div id="gamePrep">{{ $t('about.prep')}}</div>
   </div>
 </template>
+
+<style scoped>
+.about {
+  height: 100%;
+}
+#title{
+  width: 100%;
+  height: 50%;
+  background-color: #5E8C6A;
+  font-size: 2em;
+  color: #BFB35A;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+#gamePrep{
+  width: 100%;
+  height: 50%;
+  background-color: #88A65E;
+  color: #8C2318;
+  font-size: 1.5em;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
