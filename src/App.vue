b9ae09308b5c25e736feeb4ea2c2049f554bc97a
Remy.c <remychagnas@hotmail.fr>
2020-12-30 16:04:07 +0100

    Add Home page

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <About/>
     </div>
     <router-view/>
   </div>
@@ -18,15 +17,26 @@
 }
 
 #nav {
-  padding: 30px;
+  height: 10vh;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+#app {
+  height: 90vh;
 }
 
 #nav a.router-link-exact-active {
   color: #42b983;
 }
 </style>
+
+<script>
+
+import About from './views/About.vue'
+
+export default {
+  name: 'App',
+  components: {
+    About
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,66 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/images/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <md-button class="md-icon-button">
-      <md-icon>home</md-icon>
-    </md-button>
-    <span>{{ $t('home.hello') }}</span>
+    <md-steppers :md-active-step.sync="active" md-linear>
+      <md-step id="first" :md-label="$t('home.turn')"  :md-done.sync="first">
+        <Turn/>
+        <md-button class="md-raised md-primary" @click="setDone('first', 'second')">{{ $t("home.continue")}}</md-button>
+      </md-step>
+
+      <md-step id="second" :md-label="$t('home.pins')" :md-done.sync="second">
+        <Pins/>
+        <md-button class="md-raised md-primary" @click="setDone('second', 'third')">{{ $t("home.continue")}}</md-button>
+      </md-step>
+
+      <md-step id="third" :md-label="$t('home.players')" :md-done.sync="third">
+        <Players/>
+        <md-button class="md-raised md-primary" @click="setDone('third')">{{ $t("home.play")}}</md-button>
+      </md-step>
+    </md-steppers>
   </div>
 </template>
 
+<style scoped>
+  .home {
+    margin:0px;
+    height:100%;
+  }
+  .md-steppers {
+    background-color: #BFB35A !important;
+    min-height: 100%;
+  }
+  .md-stepper {
+    padding-left: 0px;
+    padding-right: 0px;
+  }
+</style>
+
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+import Turn from '../components/Turn'
+import Pins from '../components/Pins'
+import Players from '../components/PlayerListCreator'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Turn,
+    Pins,
+    Players
+  },
+  data: () => ({
+    active: 'first',
+    first: false,
+    second: false,
+    third: false
+  }),
+  methods: {
+    setDone (id, index) {
+      this[id] = true
+
+      if (index) {
+        this.active = index
+      }
+    }
   }
 }
 </script>
