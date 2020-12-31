06bba4c3b88382f71e5953e1e30c28a3a1d8d5de
tom.mansion <tomansion@yahoo.fr>
2020-12-31 12:50:55 +0100

    added more tests

--- tests/e2e/specs/gameCreationWizard.js
+++ tests/e2e/specs/gameCreationWizard.js
@@ -27,26 +27,42 @@ module.exports = {
     app.assert.visible('button[name=firstBtnValidate]')
 
     app.clearValue('input[name=turnCount]')
-    app.setValue('input[name=turnCount]', 0)
-    for (let i = 0; i < nbTurn; i++) app.click('.turn #add')
+    app.setValue('input[name=turnCount]', 1)
+    app.assert.value('input[name=turnCount]', '1')
+    for (let i = 2; i <= nbTurn; i++) {
+      app.click('.turn #add')
+      app.assert.value('input[name=turnCount]', '' + i)
+    }
     app.click('button[name=firstBtnValidate]')
 
     // Add pins
     app.assert.visible('input[name=pinsCount]')
     app.assert.visible('button[name=secondBtnValidate]')
     app.clearValue('input[name=pinsCount]')
-    app.setValue('input[name=pinsCount]', 0)
-    for (let i = 0; i < nbPins; i++) app.click('.pins #add')
+    app.setValue('input[name=pinsCount]', 5)
+    app.assert.value('input[name=pinsCount]', '5')
+    for (let i = 6; i <= nbPins; i++) {
+      app.click('.pins #add')
+      app.assert.value('input[name=pinsCount]', '' + i)
+    }
     app.click('button[name=secondBtnValidate]')
 
     // Add players
     app.assert.visible('input[name=newPlayerName]')
     app.assert.visible('button[name=thirdBtnValidate]')
+    app.assert.attributeEquals('button[name=thirdBtnValidate]', 'disabled', 'true')
+
     players.forEach((playerName, i) => {
       app.setValue('input[name=newPlayerName]', playerName)
       app.click('#PlayerListCreator #addPlayerBtn')
       app.assert.elementPresent('#PlayerListCreator #player_' + (i + 1))
     })
+
+    app.getAttribute('button[name=thirdBtnValidate]', 'disabled', result => { app.assert.equal(result.value, null) })
+    players.forEach((playerName, i) => {
+      app.assert.containsText('#PlayerListCreator #player_' + (i + 1) + ' .name', (i + 1) + ' : ' + playerName)
+    })
+
     app.click('button[name=thirdBtnValidate]')
   }
 }
