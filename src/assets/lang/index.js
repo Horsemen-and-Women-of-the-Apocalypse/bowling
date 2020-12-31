a08b6e810aef4e808dd09a80d630bea8d1395ffa
Remy.c <remychagnas@hotmail.fr>
2020-12-31 13:26:09 +0100

    update index

--- src/assets/lang/index.js
+++ src/assets/lang/index.js
@@ -1,8 +1,8 @@
 import gamepreparation from './gamepreparation'
 import turn from './turn'
 import pins from './pins'
-import header from './header'
+import gameprepheader from './gameprepheader'
 import playerListCreator from './playerListCreator'
 import merge from 'deepmerge'
 
-export default merge.all([gamepreparation, turn, pins, playerListCreator, header])
+export default merge.all([gamepreparation, turn, pins, playerListCreator, gameprepheader])
