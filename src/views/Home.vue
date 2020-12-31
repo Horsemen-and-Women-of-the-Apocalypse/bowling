b12282b97f46e9309fb35bca4682f4e7db373820
tom.mansion <tomansion@yahoo.fr>
2020-12-31 13:03:07 +0100

    added an error popup error msg

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,46 +1,79 @@
 <template>
   <div class="home">
     <md-steppers :md-active-step.sync="active" md-linear>
-      <md-step id="first" :md-label="$t('home.turn')"  :md-done.sync="first">
-        <Turn ref="turn"/>
-        <md-button name="firstBtnValidate" class="md-raised md-primary" @click="setDone('first', 'second')">{{ $t("home.continue")}}</md-button>
+      <md-step id="first" :md-label="$t('home.turn')" :md-done.sync="first">
+        <Turn ref="turn" />
+        <md-button
+          name="firstBtnValidate"
+          class="md-raised md-primary"
+          @click="setDone('first', 'second')"
+          >{{ $t("home.continue") }}</md-button
+        >
       </md-step>
 
       <md-step id="second" :md-label="$t('home.pins')" :md-done.sync="second">
-        <Pins ref="pins"/>
-        <md-button name="secondBtnValidate" class="md-raised md-primary" @click="setDone('second', 'third')">{{ $t("home.continue")}}</md-button>
+        <Pins ref="pins" />
+        <md-button
+          name="secondBtnValidate"
+          class="md-raised md-primary"
+          @click="setDone('second', 'third')"
+          >{{ $t("home.continue") }}</md-button
+        >
       </md-step>
 
       <md-step id="third" :md-label="$t('home.players')" :md-done.sync="third">
-        <Players ref="players" @playerListChange="playerListChange"/>
-        <md-button name="thirdBtnValidate" :disabled="players.length == 0" class="md-raised md-primary" @click="createGameParam()">{{ $t("home.play")}}</md-button>
+        <Players ref="players" @playerListChange="playerListChange" />
+        <md-button
+          name="thirdBtnValidate"
+          :disabled="players.length == 0"
+          class="md-raised md-primary"
+          @click="createGameParam()"
+          >{{ $t("home.play") }}</md-button
+        >
       </md-step>
     </md-steppers>
+
+    <!-- Error msg -->
+
+    <md-snackbar
+      id="errorMsg"
+      md-position="center"
+      :md-duration="4000"
+      :md-active.sync="errorMsg"
+      md-persistent
+    >
+      <span>{{ $t("home.errorMsg") }}</span>
+      <md-button class="md-primary" @click="errorMsg = false"
+        >{{ $t("home.errorMsgCloseBtn") }}</md-button
+      >
+    </md-snackbar>
   </div>
 </template>
 
 <style scoped>
-  .home {
-    margin:0px;
-    height:100%;
-  }
-  .md-steppers {
-    background-color: #BFB35A !important;
-    min-height: 100%;
-  }
-  .md-stepper {
-    padding-left: 0px;
-    padding-right: 0px;
-  }
-  .md-button {
-    background-color: #F2C45A !important;
-    color: #88A65E !important;
-    font-weight: bold;
-  }
+.home {
+  margin: 0px;
+  height: 100%;
+}
+.md-steppers {
+  background-color: #bfb35a !important;
+  min-height: 100%;
+}
+.md-stepper {
+  padding-left: 0px;
+  padding-right: 0px;
+}
+.md-button {
+  background-color: #f2c45a !important;
+  color: #88a65e !important;
+  font-weight: bold;
+}
+#errorMsg{
+  background-color: crimson;
+}
 </style>
 
 <script>
-
 import Turn from '../components/Turn'
 import Pins from '../components/Pins'
 import Players from '../components/PlayerListCreator'
@@ -59,7 +92,8 @@ export default {
     second: false,
     third: false,
     gameParam: null,
-    players: []
+    players: [],
+    errorMsg: false
   }),
   methods: {
     setDone (id, index) {
@@ -78,7 +112,11 @@ export default {
       const turn = this.$refs.turn.turnCount
       const pins = this.$refs.pins.pinsCount
 
-      this.gameParam = new GameParam(this.players, turn, pins)
+      try {
+        this.gameParam = new GameParam(this.players, turn, pins)
+      } catch (error) {
+        this.errorMsg = true
+      }
     },
     playerListChange (players) {
       this.players = players
