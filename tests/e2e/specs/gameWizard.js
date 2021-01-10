53f601f62c3063fa527577ffd5f5c392e3e5784a
Remy.c <remychagnas@hotmail.fr>
2021-01-10 19:21:39 +0100

    fix lint

--- tests/e2e/specs/gameWizard.js
+++ tests/e2e/specs/gameWizard.js
@@ -4,6 +4,6 @@ module.exports = {
   beforeEach: (browser) => browser.init(),
   afterEach: (browser) => browser.end(),
   'Wizard is present': browser => {
-    const game = browser.url('http://localhost:8000/game')
+    // TODO const game = browser.url('http://localhost:8000/game')
   }
 }
