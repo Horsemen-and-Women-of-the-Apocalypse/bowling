14aac2a6d4abe2b3c2809eb376089764c1857223
tom.mansion <tomansion@yahoo.fr>
2021-01-13 15:36:41 +0100

    added component & routes

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router'
 import GamePreparation from '../views/GamePreparation.vue'
 import ScoreboardView from '../views/ScoreboardView.vue'
 import Game from '../views/Game.vue'
+import UserManual from '../views/UserManual.vue'
 
 Vue.use(VueRouter)
 
@@ -21,6 +22,11 @@ const routes = [
     path: '/scoreboard',
     name: 'Scoreboard',
     component: ScoreboardView
+  },
+  {
+    path: '/manual',
+    name: 'UserManual',
+    component: UserManual
   }
 ]
 
@@ -30,12 +36,18 @@ const router = new VueRouter({
   routes
 })
 
+const authorizedRoutesPaths = ['/manual']
+
 router.beforeEach((to, from, next) => {
-  // Redirect user on first visit if he tries to access to another page
-  if (from.name === null && to.name !== routes[0].name) {
-    next({ name: routes[0].name })
-  } else {
+  if (authorizedRoutesPaths.includes(to.path)) {
     next()
+  } else {
+    // Redirect user on first visit if he tries to access to another page
+    if (from.name === null && to.name !== routes[0].name) {
+      next({ name: routes[0].name })
+    } else {
+      next()
+    }
   }
 })
 
---
+++ src/views/UserManual.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="UserManual">
+    <div>
+      test
+      <VueMarkdown>
+        #this is the default slot
+      </VueMarkdown>
+    </div>
+    <md-button
+      name="goToHome"
+      class="md-raised md-primary fixed-bottom md-fab md-fab-bottom-center"
+      @click="goToHome"
+    >
+      <md-tooltip md-direction="top">{{ $t("scoreBoard.back") }}</md-tooltip>
+      <md-icon>close</md-icon>
+    </md-button>
+  </div>
+</template>
+
+<script>
+import VueMarkdown from 'vue-markdown'
+
+export default {
+  name: 'ScoreboardView',
+  components: {
+    VueMarkdown
+  },
+  methods: {
+    goToHome () {
+      this.$router.push({
+        name: 'GamePreparation'
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+.scoreboardview {
+  height: 100vh;
+}
+</style>
